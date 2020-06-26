211809e7085df67df59013a413f74a6c4e27d190
Lucas Salaverry Félix <lucas.salaverry@edu.pucrs.br>
2020-06-26 14:43:30 -0300

    Controle de camera criado

---
+++ UnitedGameJam2020/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody rb_Player;
+    [SerializeField]
+    private float cameraHeight;
+    [SerializeField]
+    private float cameraTilt;
+    private Camera camComponent;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camComponent = this.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        camComponent.transform.position = new Vector3(rb_Player.transform.position.x, cameraHeight, rb_Player.transform.position.z);
+    }
+}
---
+++ UnitedGameJam2020/Assets/Scripts/Movement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
