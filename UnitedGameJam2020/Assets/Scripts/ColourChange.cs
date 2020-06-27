25eefe06bd4f12aa3eb1862960c8fc8e7bf33f46
Lucas Salaverry Félix <lucas.salaverry@edu.pucrs.br>
2020-06-27 13:18:13 -0300

    Colour Change

---
+++ UnitedGameJam2020/Assets/Scripts/ColourChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourChange : MonoBehaviour
+{
+
+    public Material ChangeMaterial;
+ 
+    
+    void OnCollisionEnter(Collision other) {
+        Debug.Log("Colisao");
+        if(other.gameObject.tag == "Obstacles")
+            other.gameObject.GetComponent<MeshRenderer>().material = ChangeMaterial;
+    }
+    
+
+
+}
