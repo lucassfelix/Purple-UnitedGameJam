b1675cb31183a62ee9771a7ae7133749eb76dddc
Lucas Salaverry Félix <lucas.salaverry@edu.pucrs.br>
2020-06-28 01:07:27 -0300

    Victory implemented

--- UnitedGameJam2020/Assets/Scripts/ColourChange.cs
+++ UnitedGameJam2020/Assets/Scripts/ColourChange.cs
@@ -1,24 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 public class ColourChange : MonoBehaviour
 {
 
-    public Material ChangeMaterial;
+    public Material changeMaterial;
+    public Material winChangeMaterial;
 
-    
+    public GameController gameController;
     
     void OnCollisionEnter(Collision other) {
-        Debug.Log("Colisao");
-        if(other.gameObject.tag == "Obstacles")
+         if(other.gameObject.tag ==  "Obstacles")
         {
             gameObject.GetComponent<ParticleSystem>().Play();
-            other.gameObject.GetComponent<MeshRenderer>().material = ChangeMaterial;
+            other.gameObject.GetComponent<MeshRenderer>().material = changeMaterial;
         }
         else if (other.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings);
+            
+            changeMaterial = winChangeMaterial;
+            gameController.WinLevel(other.GetContact(0).point);
         }
     }
     
